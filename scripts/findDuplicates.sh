5e1a5eef68f46342530dd2525276c282c906f6eb
mangal <mangal@mangal-HP-EliteBook-840-G1>
2016-09-18 22:22:15 +0530

    NewSet

--- scripts/findDuplicates.sh
+++ scripts/findDuplicates.sh
@@ -1,10 +1,20 @@
-for line in `egrep ":2015_|:2016_" safe/dumpText.txt |awk -F: '{print $4":"$5}'`; 
-do 
-count=`grep -c $line safe/dumpText.txt` ; 
+BASE_DIR=..
+INPUT=$BASE_DIR/output/dumpText.txt
+OUTPUT=$BASE_DIR/output/DuplicateFiles.txt
+
+while read -r line
+do
+#set -x
+photo=`echo $line|awk -F: '{print $4}'`
+timestamp=`echo $line|awk -F: '{print $5}'`
+count=`grep -c "$photo:$timestamp" $INPUT`  
 if [ $count -gt 1 ]
 then
-echo $line
+export OIFS=$IFS
+export IFS=:
+printf "%s\n" "$line"
+export IFS=$OIFS
 fi
-done>duplicates.txt
-
-cat duplicates.txt |sort |uniq >duplicates_1.txt
+#set +x
+done<$INPUT>tmp_duplicates.txt
+cat tmp_duplicates.txt |sort |uniq >$OUTPUT
